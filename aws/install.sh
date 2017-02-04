39be2b21808793cb0dcefc685c7efc55a1df985a
Ubuntu <ubuntu@ip-172-31-47-225.ec2.internal>
2017-02-04 20:24:14 +0000

    Setting up basic MQTT server completed

--- aws/install.sh
+++ aws/install.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# install dependancy of mosquitto
+sudo sh -c "echo 'deb http://us.archive.ubuntu.com/ubuntu vivid main universe' >> /etc/apt/sources.list"
+sudo apt-get update
+sudo apt-get install libwebsockets3
+
+# install mosquitto
 sudo wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
 sudo apt-key add mosquitto-repo.gpg.key
 cd /etc/apt/sources.list.d/
-sudo wget http://repo.mosquitto.org/debian/mosquitto-jessie.list  # In case of you use debian Jessie (ubuntu 14.04 or higher). If you use stretch (or ubuntu 16.04) download mosquitto-stretch instead.
+sudo wget http://repo.mosquitto.org/debian/mosquitto-stretch.list  # In case of you use debian Jessie (ubuntu 14.04 or higher). If you use stretch (or ubuntu 16.04) download mosquitto-stretch instead.
 sudo apt-get update
 sudo apt-get install mosquitto
 sudo apt-get install python-setuptools python-dev build-essential
 sudo apt-get install python-pip
 sudo pip install paho-mqtt
-sudo apt-get install python-dev libmysqlclient-dev
-sudo apt-get install python-mysql.connector
