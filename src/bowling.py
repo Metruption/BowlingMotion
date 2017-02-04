458218651f05ed397ee61cd97e92d5335cd8ffa9
Aaron Thomas <1d20m6@gmail.com>
2017-02-04 14:31:44 -0500

    Added partial implementation of roll_ball
    Still need to write all the math intensive code.
    @todo(aaron) relearn trig

--- src/bowling.py
+++ src/bowling.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import math
+#distance units are in inches
+#weight units are in kilos
 
 PIN_LOCATIONS = [
 [720,30],		#pin 1
@@ -21,10 +23,11 @@ PIN_LOCATIONS = [
 class Pin:
 	"""These poor guys get knocked around a lot."""
 
-	knock_resistance = 10
-	height = 15
-	def __init__(self, position):
-		self.position = position
+	radius = 2.25 #our pins are going to basically be cylinders
+	mass = 1.5 #kilos
+	height = 15 #inches
+	def __init__(self, pos):
+		self.xpos, self.ypos = pos[1], pos[1]
 		self.tip = 1 #1 = standing up, 0 = horizontal
 		self.angle = 0 	#WE ARE USING DEGREES, NOT RADIANS
 						#0 is pointed NORTH
@@ -35,24 +38,62 @@ class Pin:
 class Lane:
 	"""
 	A bowling lane with pins, and a ball.
+	The 'physics engine'
 	"""
 	ball_mass = 10 #this is an @arbitrary number, adjust as needed
+	ball_radius = 4.25
 
 	def __init__(self):
-		pins = self.ten_pins()
+		start_frame()
+
+	def start_frame():
+		self.pins = self.ten_pins()
 
 	def ten_pins():
 		return [Pin(PIN_LOCATIONS[i]) for i in range(10)] #pin indicies are 1-10, because thats how bowling does it
 
-	def roll_ball(ball_x, ball_vel, ball_spin, ball_angle):
+	def distance(x1,y1,x2,y2):
 		'''
 		@params:
-			ball_x is a float between 9.25 and 50.75 inclusive
+			x1 and y1 are the (x,y) coords of one points
+			x2 and y2 are the (x,y) coords of a second point
+
+		returns the distance between the two points
+		'''
+		return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+	def roll_ball(ball_x, ball_vel, ball_spin=None, ball_angle):
+		def split_vel():
+			ball_xvel = '@todo(someone) code this' #not aaron, he doesnt know trig
+			ball_yvel = '@todo(someone) code this' #not arrow he doesnt know trig
+
+		def update_bal_pos(ball_x, ball_y, ball_xvel, ball_yvel):
+			ball_x = ball_x + ball_xvel
+			ball_y = ball_y + ball_yvel
+		'''
+		@params:
+			ball_x is a float between 9.25 and 50.75 inclusive (you cant start out in the gutter)
 			ball_vel is how fast the ball is rolling
 			ball_spin is the spin on the ball (to make it curve) (maybe don't use this)
 			ball_angle is the angle that the ball is going in
 		'''
-		pass #@todo(someone) code this
+		ball_y = 0.0
+		pins_knocked = []
+
+		continue_simulation = True
+		while continue_simulation:
+			#render_image() ????
+			split_vel()
+			update_bal_pos()
+
+			if ball_y > 50.75 or ball_y < 9.25: #check if the ball is in the gutters
+				continue_simulation = False
+				#this is a gutter ball!
+
+			pin_distances = [distance(ball_x, ball_y, pin.xpow, pin.ypos) for pin in pins]
+			for pin_distance in pin_distances:
+				if pin_distance < ball_radius + Pin.radius:
+					#@todo(aaron): figure out how to make the ball impact the pin
 
 class BowlingGame:
 	"""
@@ -60,3 +101,6 @@ class BowlingGame:
 	"""
 	#do we need this?
 	#@todo(aaron) answer the above question
+	#the answer is yes!
+	#it has a bowling lane
+	#it keeps track of scoring
